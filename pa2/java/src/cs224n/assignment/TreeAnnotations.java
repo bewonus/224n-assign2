1ef3bd180f7e5515ec0c2aef0dc9f9a686767dd9
Brandon Taylor Ewonus <bewonus@corn37.stanford.edu>
2014-10-21 14:58:34 -0700

    added second and third order vertical markovization annotations (trees print fine, but binarizing might be buggy)

--- pa2/java/src/cs224n/assignment/TreeAnnotations.java
+++ pa2/java/src/cs224n/assignment/TreeAnnotations.java
@@ -26,9 +26,60 @@ public class TreeAnnotations {
 		// TODO : mark nodes with the label of their parent nodes, giving a second
 		// order vertical markov process
 
+    Tree<String> verticalTree = verticalMarkovization(unAnnotatedTree.deepCopy(), null);
+    Tree<String> thirdVerticalTree = thirdVerticalMarkovization(unAnnotatedTree.deepCopy(), null, null);
+    System.out.println("original tree: ");
+    System.out.println("" + unAnnotatedTree);
+    System.out.println("second order vertical tree: ");
+    System.out.println("" + verticalTree);
+    System.out.println("third order vertical tree: ");
+    System.out.println("" + thirdVerticalTree);
+
 		return binarizeTree(unAnnotatedTree);
 
 	}
+
+  /**
+   * Second order vertical markovization. Marks all non-terminal nodes (including preterminals,
+   * but not the ROOT or terminals) with the label of their parent tag.
+   */
+  private static Tree<String> verticalMarkovization(Tree<String> tree, String parentTag) {
+    if (tree.getChildren().isEmpty()) {
+      return new Tree<String>(tree.getLabel());
+    }
+    List<Tree<String>> children = new ArrayList<Tree<String>>();
+    for (Tree<String> child : tree.getChildren()) {
+      children.add(verticalMarkovization(child, tree.getLabel()));
+    }
+    if (parentTag != null) {
+      tree.setLabel(tree.getLabel() + "^" + parentTag);
+    }
+
+    return new Tree<String>(tree.getLabel(), children);
+  }
+
+  /**
+   * Second order vertical markovization. Marks all non-terminals (including preterminals,
+   * but not ROOT or terminals) with their parent tag as well.
+   */
+  private static Tree<String> thirdVerticalMarkovization(Tree<String> tree, String parentTag, String grandParentTag) {
+    if (tree.getChildren().isEmpty()) {
+      return new Tree<String>(tree.getLabel());
+    }
+    List<Tree<String>> children = new ArrayList<Tree<String>>();
+    for (Tree<String> child : tree.getChildren()) {
+      children.add(thirdVerticalMarkovization(child, tree.getLabel(), parentTag));
+    }
+    if (parentTag != null) {
+      tree.setLabel(tree.getLabel() + "^" + parentTag);
+    }
+    if (grandParentTag != null) {
+      tree.setLabel(tree.getLabel() + "^" + grandParentTag);
+    }
+
+    return new Tree<String>(tree.getLabel(), children);
+  }
+
 
 	private static Tree<String> binarizeTree(Tree<String> tree) {
 		String label = tree.getLabel();
